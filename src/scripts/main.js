c06484d09784353c69f37b9e68f9b86d261b9515
David Storey <david@lostincode.co.uk>
2015-07-16 11:57:30 +0100

    Refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,24 @@
 (function(){
 	'use strict';
 
-	var controls = new Ractive({
-		el:       '#controls',
-		template: '#controlsTemplate',
-		data: {
+	function createView(id, data) {
+		data = data ? data : {};
+
+		return new Ractive({
+			el:       '#' + id,
+			template: '#' + id + 'Template',
+			data:     data
+		})
+	}
+
+	var
+		screens = createView('screens', { current: 'intro' }),
+		intro = createView('intro'),
+		main = createView('main'),
+		outro = createView('outro'),
+		controls = createView('controls', {
 			score: 0,
 			currentQuestion: 0,
 			totalQuestions: 0
-		}
-	});
+		});
 }());
